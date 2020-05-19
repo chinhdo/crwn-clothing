f1f98161c42a7be51ffc531f8a462e12a01b004f
Chinh Do <cdo@vienxu.com>
2020-05-18 23:25:29 -0400

    End of 69

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
-  return <div><HomePage/></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
