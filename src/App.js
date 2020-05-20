6980d594b613eb139b5ff24a7c7abfe8d69cf4ef
Chinh Do <cdo@vienxu.com>
2020-05-19 20:47:19 -0400

    Header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <div>
+    <div>      
       <BrowserRouter>
+        <Header />
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
       </BrowserRouter>
